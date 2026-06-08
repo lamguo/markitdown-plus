c57901372b9b10c512e4dded708d5cbc00885e62
lamguo <lamguo111@gmail.com>
2026-06-08 07:42:46 -0400

    Fix I001: unsorted imports

--- src/markitdown_plus/__init__.py
+++ src/markitdown_plus/__init__.py
@@ -5,8 +5,8 @@ Microsoft MarkItDown powered document pipelines.
 """
 
 from .__about__ import __version__
-from .cleaner import clean_markdown
 from .chunker import chunk_markdown
+from .cleaner import clean_markdown
 from .converter import PlusConverter
 
 __all__ = ["PlusConverter", "clean_markdown", "chunk_markdown", "__version__"]
